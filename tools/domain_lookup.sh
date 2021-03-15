c64251dabbcb5797425a823f2202e3a2523a074b
damon hunka <hunkadamon@gmail.com>
2021-03-15 14:52:45 -0700

    fix

--- tools/domain_lookup.sh
+++ tools/domain_lookup.sh
@@ -5,7 +5,7 @@ echo V00863155
 echo SENG 460 Lab Assignment 
 echo ________________________
 
-if [-z "$1"]
+if [ -z "$1" ]
 then
     echo Domain input not passed into script, Enter now: 
     read -p 'Domain: ' domainIn
