8bef753060c4cea1c5c2e4f0d0c2d642b0ad44f0
damon hunka <hunkadamon@gmail.com>
2021-03-15 16:41:51 -0700

    added further

--- tools/domain_lookup.sh
+++ tools/domain_lookup.sh
@@ -3,22 +3,39 @@ echo __________________________
 echo DAMON HUNKA             
 echo V00863155               
 echo SENG 460 Lab Assignment 
-echo ________________________
+echo __________________________
 
 if [ -z "$1" ]
-then
-    echo Domain input not passed into script, Enter now: 
-    read -p 'Domain: ' domainIn
-
+  then
+    echo Domain input not passed into script, 
+    read -p 'Enter Domain now: ' domainIn
 else
     domainIN=$1
 fi
 
 echo 
-echo Entered domain: $domainIN
+echo "Entered domain: $domainIN"
 echo 
-echo Gathering Information on $domainIN
+echo "Gathering Information on $domainIN ..."
 echo 
 
-`whois $domainIN`
+DomainProcessed=${domainIN#www.}
+
+echo "Fetching Registrar server record ..."
+
+whoisServ=`whois $DomainProcessed | grep 'Registrar WHOIS Server:' | head -n1 | awk '{print $4'}`
+
+if [ $whoisServ ]; 
+then
+    echo "Registrar WHOIS Server:"$whoisServ
+    `whois -h $whoisServ $DomainProcessed` >> result.txt
+else
+    `whois $DomainProcessed` >> result.txt
+fi
+
+echo "Information Gathered on $DomainProcessed!"
+echo __________________________
+
+domainname=$(grep 'Domain Name:' result.txt| cut -d: -f2-)
+echo "Domain Name:" $domainname
 
