bfe86c5a6fe35031db993b997220c5fbd9c46988
damon hunka <hunkadamon@gmail.com>
2021-03-15 13:17:48 -0700

    Added domain_lookup script

---
+++ tools/domain_lookup.sh
@@ -0,0 +1,22 @@
+
+echo __________________________
+echo |DAMON HUNKA             |
+echo |V00863155               |
+echo |SENG 460 Lab Assignment |
+echo |________________________|
+
+echo Domain input not passed into script, Enter now: 
+local domainIN ="$1"
+
+if [-z "domainIN"]
+then
+    read -p 'Domain: ' domainIn
+
+    echo 
+    echo Entered domain: $domainIN
+    echo 
+f1
+
+whois domainIN
+
+
