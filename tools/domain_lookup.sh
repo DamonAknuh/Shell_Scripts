490338bd585cbf93a92b467f8942757cdb1c1016
damon hunka <hunkadamon@gmail.com>
2021-03-17 11:08:08 -0700

    Final Final Final Finality formatting

--- tools/domain_lookup.sh
+++ tools/domain_lookup.sh
@@ -40,19 +40,16 @@ fi
 
 # general info
 #registrant
-netName=$(grep -i 'netName:' IPresults.txt| cut -d: -f2-)
+netName=$(grep -i 'netName:' IPresults.txt|  head -n1 | cut -d: -f2-)
 orgName=$(grep -i 'orgName:' IPresults.txt| cut -d: -f2-)
 CIDR=$(grep -i 'CIDR:' IPresults.txt| head -n1 | cut -d: -f2-)
-city=$(grep 'Tech Organization:' IPresults.txt| cut -d: -f2-)
-stateProv=$(grep  -i 'StateProv:' IPresults.txt| cut -d: -f2-)
-country=$(grep  -i 'Country:' IPresults.txt| cut -d: -f2-)
 orgAbuseEmail=$(grep  -i 'OrgAbuseEmail:' IPresults.txt| cut -d: -f2-)
 orgAbusePhone=$(grep  -i 'OrgAbusePhone:' IPresults.txt| cut -d: -f2-)
 
 #DNS
 if [ $dServer ]; 
 then
-    regName=$(grep 'Registrant Name:' DNSresult.txt| cut -d: -f2-)
+    regName=$(grep 'Registrant Name:' DNSresult.txt|  head -n1 | cut -d: -f2-)
     dName=$(grep -i 'Domain Name:' DNSresult.txt| cut -d: -f2-)
     dHostorg=$(grep 'Registrar:' DNSresult.txt| cut -d: -f2-)
     dHostAbuseEmail=$(grep 'Registrar Abuse Contact Email:' DNSresult.txt| cut -d: -f2-) 
@@ -66,12 +63,12 @@ fi
 wHostIP=`traceroute $DomainProcessed -m 60 -w 10 -q 1 -N 32 -n| grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | tail -n1`
 wHostorg=`whois $wHostIP -H| grep 'Organization:' | head -n1 | cut -d: -f2-`
 wHostCIDR=`whois $wHostIP -H| grep 'CIDR:' |  head -n1 | cut -d: -f2-`
-wHostCity=`whois $wHostIP -H| grep -i 'City:' | cut -d: -f2-`
+wHostCity=`whois $wHostIP -H| grep -i 'City:'|  head -n1  | cut -d: -f2-`
 wHostRegion=`whois $wHostIP -H| grep -i 'StateProv:' |  head -n1 | cut -d: -f2-`
 wHostCountry=`whois $wHostIP -H| grep -i 'Country:' |  head -n1 | cut -d: -f2-`
 wNetName=`whois $wHostIP -H| grep -i 'netname:' | cut -d: -f2-`
-wHostAbuseEmail=`whois $wHostIP -H| grep 'OrgAbuseEmail:' | cut -d: -f2-`
-wHostAbusePhone=`whois $wHostIP -H| grep 'OrgAbusePhone:' | cut -d: -f2-`
+wHostAbuseEmail=`whois $wHostIP -H| grep 'OrgAbuseEmail:'|  head -n1  | cut -d: -f2-`
+wHostAbusePhone=`whois $wHostIP -H| grep 'OrgAbusePhone:'|  head -n1  | cut -d: -f2-`
 
 #Network
 ispOrg=$(curl -s ipinfo.io/$IPaddress | grep 'org' | cut -d: -f2- | cut -d\" -f2 )
